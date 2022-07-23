8a85328d4bbc2b051d9a5fc90514b143fed0191e
isaaclal124 <isaaclal124@gmail.com>
2022-07-23 19:57:45 -0400

    version.py: added space in comment

--- version.py
+++ version.py
@@ -3,4 +3,4 @@ import sys
 print(sys.version)
 print(sys.executable)
 
-#Visual Studio Code (Mac) - Setting up a Python Development Environment and Complete Overview (30:27)
+# Visual Studio Code (Mac) - Setting up a Python Development Environment and Complete Overview (30:27)
