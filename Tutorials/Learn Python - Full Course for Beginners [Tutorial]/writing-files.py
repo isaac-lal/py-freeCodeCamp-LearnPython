184ffa92f4d7f87647f0c1977a0272847481d599
isaaclal124 <isaaclal124@gmail.com>
2022-07-28 23:09:53 -0400

    progressed python tutorial: stopped at 3:21:28

---
+++ Tutorials/Learn Python - Full Course for Beginners [Tutorial]/writing-files.py
@@ -0,0 +1,4 @@
+# freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (3:21:28)
+
+# 3:21:28
+# https://www.youtube.com/watch?v=rfscVS0vtbw
--- Tutorials/freeCodeCamp/Learn Python - Full Course for Beginners [Tutorial]/while-loop.py
+++
@@ -1,3 +0,0 @@
-# freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (2:14:14)
-
-# 2:14:14
