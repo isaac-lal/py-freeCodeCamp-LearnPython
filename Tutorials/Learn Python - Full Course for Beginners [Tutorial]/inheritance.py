a0c58b1c216bbc66150c2514e986815dccee9e60
isaaclal124 <isaaclal124@gmail.com>
2022-08-01 12:09:34 -0400

    added chinese chef file

--- Tutorials/Learn Python - Full Course for Beginners [Tutorial]/inheritance.py
+++ Tutorials/Learn Python - Full Course for Beginners [Tutorial]/inheritance.py
@@ -1,3 +1,8 @@
 # freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (4:12:38)
 
 from chef import Chef
+from chinese_chef import ChineseChef
+
+myChef = Chef()
+myChef.make_chicken()
+myChef.make_special_dish()
