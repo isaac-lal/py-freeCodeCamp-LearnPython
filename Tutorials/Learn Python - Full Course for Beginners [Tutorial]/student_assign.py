170d62b6400f74189a9d5e19d001a10b2f288450
isaaclal124 <isaaclal124@gmail.com>
2022-08-01 11:26:49 -0400

    learned how to model classes and create datatypes

---
+++ Tutorials/Learn Python - Full Course for Beginners [Tutorial]/student_assign.py
@@ -0,0 +1,9 @@
+from student_class import Student
+
+Isaac = Student("Isaac", "Computer Science", 3.4, False)
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+
+print(Isaac.major)
+print(student1.gpa)
+print(student2.gpa)
