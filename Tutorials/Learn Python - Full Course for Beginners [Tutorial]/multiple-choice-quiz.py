426627d678dbc674968056f33eb9b08e2f04938a
isaaclal124 <isaaclal124@gmail.com>
2022-08-01 11:40:27 -0400

    created question.py

--- Tutorials/Learn Python - Full Course for Beginners [Tutorial]/multiple-choice-quiz.py
+++ Tutorials/Learn Python - Full Course for Beginners [Tutorial]/multiple-choice-quiz.py
@@ -1,6 +1,6 @@
 # freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (3:57:36)
 
-question_prompts = [
+question_prompts = [ # question prompts for multiple choice quiz
     "What color are honeycrisp apples?\n(a) Red\n(b) Green\n(c) Purple\n(d) Orange\n\n",
     "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
     "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
