9874c887a432688f12113d7a525614897e43dbce
isaaclal124 <isaaclal124@gmail.com>
2022-08-01 11:49:10 -0400

    created multiple-choice-quiz

--- Tutorials/Learn Python - Full Course for Beginners [Tutorial]/multiple-choice-quiz.py
+++ Tutorials/Learn Python - Full Course for Beginners [Tutorial]/multiple-choice-quiz.py
@@ -1,5 +1,7 @@
 # freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (3:57:36)
 
+from question import Question
+
 question_prompts = [ # question prompts for multiple choice quiz
     "What color are honeycrisp apples?\n(a) Red\n(b) Green\n(c) Purple\n(d) Orange\n\n",
     "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
@@ -7,5 +9,17 @@ question_prompts = [ # question prompts for multiple choice quiz
 ]
 
 questions = [
-    Question(question_prompts[0])
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
 ]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(questions)
