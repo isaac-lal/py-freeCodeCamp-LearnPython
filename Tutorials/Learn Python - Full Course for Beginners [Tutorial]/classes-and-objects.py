106228e9e1d5845e208cbae1d11838fb61235a1d
isaaclal124 <isaaclal124@gmail.com>
2022-08-01 11:20:28 -0400

    classes & objects: changed name

--- Tutorials/Learn Python - Full Course for Beginners [Tutorial]/classes-and-objects.py
+++
@@ -1,2 +0,0 @@
-# freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (3:43:57)
-
---
+++ Tutorials/Learn Python - Full Course for Beginners [Tutorial]/student.py
@@ -0,0 +1,12 @@
+# freeCodeCamp.org: Learn Python - Full Course for Beginners [Tutorial] (3:43:57)
+
+#Learning Classes & Objects
+
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+        
